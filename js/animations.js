7380e9bde6e54596628b86d4108f74110f3b40fe
SufianHamdan <sufian_hamdan@yahoo.com>
2021-04-04 15:43:16 +0300

    Design-in-process3

--- js/animations.js
+++ js/animations.js
@@ -1,15 +1,34 @@
-let myIndex = 0;
-carousel();
+// let myIndex = 0;
+// carousel();
 
-function carousel() {
+// function carousel() {
+//   let i;
+//   let x = document.getElementsByClassName('mySlides');
+//   for (i = 0; i < x.length; i++) {
+//     x[i].style.display = 'none';
+//   }
+//   myIndex++;
+//   if (myIndex > x.length) {myIndex = 1;}
+//   x[myIndex-1].style.display = 'block';
+//   setTimeout(carousel, 3000); // Change image every 2 seconds
+// }
+
+let slideIndex = 0;
+showSlides();
+
+function showSlides() {
   let i;
-  let x = document.getElementsByClassName('mySlides');
-  for (i = 0; i < x.length; i++) {
-    x[i].style.display = 'none';
+  let slides = document.getElementsByClassName('mySlides');
+  let dots = document.getElementsByClassName('dot');
+  for (i = 0; i < slides.length; i++) {
+    slides[i].style.display = 'none';
   }
-  myIndex++;
-  if (myIndex > x.length) {myIndex = 1;}
-  x[myIndex-1].style.display = 'block';
-  setTimeout(carousel, 2000); // Change image every 2 seconds
+  slideIndex++;
+  if (slideIndex > slides.length) {slideIndex = 1}
+  for (i = 0; i < dots.length; i++) {
+    dots[i].className = dots[i].className.replace(' active', '');
+  }
+  slides[slideIndex-1].style.display = 'block';
+  dots[slideIndex-1].className += ' active';
+  setTimeout(showSlides, 3000); // Change image every 2 seconds
 }
-
