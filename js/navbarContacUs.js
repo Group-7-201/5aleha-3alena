9b66612d9d403689ef8419fd427d142f1c281af8
qusai <qosay.omar124@gmail.com>
2021-04-06 15:31:14 +0300

    updates-contact-people

---
+++ js/navbarContacUs.js
@@ -0,0 +1,14 @@
+// alert('heeeey');
+
+const header = document.getElementById('navbar');
+window.onscroll = function () {
+
+  let top = window.scrollY;
+  console.log(top);
+  if (top >= 100) {
+    header.classList.add('active');
+  }
+  else {
+    (header.classList.remove('active'));
+  }
+};
