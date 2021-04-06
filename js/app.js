3632cb87cda7344c7a11500dfbab5e0917c626de
qusai <qosay.omar124@gmail.com>
2021-04-06 12:10:12 +0300

    update

--- js/app.js
+++ js/app.js
@@ -1,8 +1,8 @@
 'use strict';
 
 // Places Name Arry:
-const placesName = ['alarays pool', 'jordan river', 'kingtalal dom', 'wadi hidan', 'wadi mujib', 'wadi rum', 'dead sea','maen hot',
-  'Temple Artemis','Temple Hercules','Treasury Petra','Umm Qais'];
+const placesName = ['alarays pool', 'jordan river', 'kingtalal dom', 'wadi hidan', 'wadi mujib', 'wadi rum', 'dead sea', 'maen hot',
+  'Temple Artemis', 'Temple Hercules', 'Treasury Petra', 'Umm Qais'];
 
 // Global Vaiables:
 let reStoreValue = null;
@@ -15,7 +15,7 @@ function Places(name, info) {
   this.path = `./images/${name}.jpg`;
   this.placeInf = info;
   this.placeRate = 0;
-  this.rate=0;
+  this.rate = 0;
   reStore();
   if (reStoreValue === null) {
     Places.all.push(this);
@@ -35,9 +35,9 @@ let place7 = new Places(placesName[6], 'the geological wonder of theDead Seais o
 let place8 = new Places(placesName[7], 'When feeling weary, the Ma’in Hot Springs are your perfect remedy. The sight is centered between Madaba and the Dead Sea. Indulge in the hot, mineral waters and enjoy the breeze of nature. For a night’s stay, check in the Ma’in Hot Springs Spa & Resort for further pampering.');
 let place9 = new Places(placesName[8], 'Ruins of the legendary and iconic Temple of Artemis are still standing tall at the highest point in the city of Jerash.');
 let place10 = new Places(placesName[9], 'Dating back to the time of the erection of the Roman Theater, the Temple of Hercules is located at the top hill of the glorious Citadel of Amman, and is also visible from downtown.');
-let place11 = new Places(placesName[10],'Surprisingly, the rose-city has much more to offer, and tribute must be paid to Petra‘s most iconic monument, Al-Khazheh or the Treasury. This signature jaw-dropping sight will meet you the moment you finish walking through the 1200-meter Al-Siq.');
+let place11 = new Places(placesName[10], 'Surprisingly, the rose-city has much more to offer, and tribute must be paid to Petra‘s most iconic monument, Al-Khazheh or the Treasury. This signature jaw-dropping sight will meet you the moment you finish walking through the 1200-meter Al-Siq.');
 let place12 = new Places(placesName[11], 'Umm Qais is a historical and natural sight located in the north of Jordan, known for its ancient ruins of Gadara. Umm Qais has optimal weather and spreading natural landscapes almost all year long.');
-Places.all.push = [place1, place2, place3, place3, place4, place5, place6, place7,place8,place9,place10,place11,place12];
+Places.all.push = [place1, place2, place3, place3, place4, place5, place6, place7, place8, place9, place10, place11, place12];
 
 // Store Function in Local Storage:
 function store() {
@@ -95,12 +95,12 @@ function reStore() {
 
 let placesSection = document.getElementById('places');
 
-let normalPlaces=JSON.parse(localStorage.getItem('places')) || [];
+let normalPlaces = JSON.parse(localStorage.getItem('places')) || [];
 
-for (let i=0; i<normalPlaces.length; i++){
-  let placesObj=new Places (normalPlaces[i].name,normalPlaces[i].placeInfo);
+for (let i = 0; i < normalPlaces.length; i++) {
+  let placesObj = new Places(normalPlaces[i].name, normalPlaces[i].placeInfo);
 }
-let paragraphArray= [
+let paragraphArray = [
   'This is theblessing of themost beautiful ponds inJordanas it exists between themountain range islocated in thevillage kingcomposed scene aesthetically pleasing in thespring, his pool hostsraretypes ofanimalsandbirds, such as: otter,fish owl,river and waterturtlesIt is one of the rare and endangered species',
   'Stand beside the Jordan River where it’s thought Jesus was baptized on this private 4-hour trip from Amman. Travel to the ‘Bethany Beyond the Jordan’ baptism site, also known as El-Maghtas, and tour the riverbanks and ruins of ancient churches, wells, and baptismal pools.',
   'The dam is 92 meters high, which make the flood control system look like a pretty nice ski jump, the lake can contain 90 millions of water cubic meters',
@@ -111,29 +111,29 @@ let paragraphArray= [
 ];
 
 
-function placesRender(){
-  for (let i=0; i<Places.all.length; i++){
+function placesRender() {
+  for (let i = 0; i < Places.all.length; i++) {
     // let divEl=document.getElementById('');
-    let divEl=document.createElement('div');
+    let divEl = document.createElement('div');
     placesSection.appendChild(divEl);
-    let item1=document.createElement('img');
+    let item1 = document.createElement('img');
     divEl.appendChild(item1);
-    item1.src=Places.all[i].path;
-    item1.alt=Places.all[i].name;
-    item1.id=`${Places.all[i].name}`;
-    let titleEl=document.createElement('h3');
+    item1.src = Places.all[i].path;
+    item1.alt = Places.all[i].name;
+    item1.id = `${Places.all[i].name}`;
+    let titleEl = document.createElement('h3');
     divEl.appendChild(titleEl);
-    titleEl.textContent=`${Places.all[i].name}`;
-    titleEl.id=`${Places.all[i].name}`;
-    let parEl=document.createElement('p');
+    titleEl.textContent = `${Places.all[i].name}`;
+    titleEl.id = `${Places.all[i].name}`;
+    let parEl = document.createElement('p');
     titleEl.appendChild(parEl);
-    parEl.textContent=`${paragraphArray[i]}`;
+    parEl.textContent = `${paragraphArray[i]}`;
     rating();
   }
 }
 placesRender();
 
-Places.prototype.rating = function(){
+Places.prototype.rating = function () {
 
   //   <div class="center">
   //   <div class="stars">
@@ -151,85 +151,85 @@ Places.prototype.rating = function(){
   //   </div>
   // </div>
 
-  let div1El=document.createElement('div');
+  let div1El = document.createElement('div');
   placesSection.appendChild(div1El);
-  div1El.className='center';
+  div1El.className = 'center';
 
-  let div2El=document.createElement('div');
+  let div2El = document.createElement('div');
   div1El.appendChild(div2El);
-  div2El.className='stars';
+  div2El.className = 'stars';
 
-  let unput1=document.createElement('input');
-  unput2.name='five';
-  unput2.type='radio';
+  let unput1 = document.createElement('input');
+  unput2.name = 'five';
+  unput2.type = 'radio';
   div2El.appendChild(unput1);
-  unput1.id= this.name +'_five';
+  unput1.id = this.name + '_five';
 
-  let unput2=document.createElement('input');
-  unput1.name='foure';
-  unput1.type='radio';
+  let unput2 = document.createElement('input');
+  unput1.name = 'foure';
+  unput1.type = 'radio';
   div2El.appendChild(unput2);
-  unput2.id=this.name+'_four';
+  unput2.id = this.name + '_four';
 
-  let unput3=document.createElement('input');
-  unput3.name='three';
-  unput3.type='radio';
+  let unput3 = document.createElement('input');
+  unput3.name = 'three';
+  unput3.type = 'radio';
   div2El.appendChild(unput3);
-  unput3.id=this.name+'_three';
+  unput3.id = this.name + '_three';
 
-  let unput4=document.createElement('input');
-  unput4.name='two';
-  unput4.type='radio';
+  let unput4 = document.createElement('input');
+  unput4.name = 'two';
+  unput4.type = 'radio';
   div2El.appendChild(unput4);
-  unput4.id=this.name+'_two';
+  unput4.id = this.name + '_two';
 
-  let unput5=document.createElement('input');
-  unput5.name='one';
-  unput5.type='radio';
+  let unput5 = document.createElement('input');
+  unput5.name = 'one';
+  unput5.type = 'radio';
   div2El.appendChild(unput5);
-  unput5.id=this.name+'_one';
+  unput5.id = this.name + '_one';
 
 
-  unput1.textContent='Thanks for rating us!';
-  let div4El=document.createElement('div');
+  unput1.textContent = 'Thanks for rating us!';
+  let div4El = document.createElement('div');
   div1El.appendChild(div4El);
-  div4El.className='edit';
-  div4El.textContent='EDIT';
-  let div5El=document.createElement('div');
+  div4El.className = 'edit';
+  div4El.textContent = 'EDIT';
+  let div5El = document.createElement('div');
   div1El.appendChild(div5El);
-  div5El.className='star-widget';
-  for (let i=5; i>0; i--){
-    let inputEl=document.createElement('input');
+  div5El.className = 'star-widget';
+  for (let i = 5; i > 0; i--) {
+    let inputEl = document.createElement('input');
     div5El.appendChild(inputEl);
-    inputEl.type='radio';
-    inputEl.name='rate';
-    inputEl.id=`${this.name}-${i}`;
-    let labelEl= document.createElement('label');
+    inputEl.type = 'radio';
+    inputEl.name = 'rate';
+    inputEl.id = `${this.name}-${i}`;
+    let labelEl = document.createElement('label');
     div5El.appendChild(labelEl);
-    labelEl.for=`${this.name}-${i}`;
-    labelEl.className='fas fa-star';
+    labelEl.for = `${this.name}-${i}`;
+    labelEl.className = 'fas fa-star';
 
   }
-  let formEl=document.createElement('form');
+  let formEl = document.createElement('form');
   div5El.appendChild(formEl);
-  formEl.action='#';
-  let headerEl=document.createElement('header');
+  formEl.action = '#';
+  let headerEl = document.createElement('header');
   div5El.appendChild(headerEl);
-  let div6El=document.createElement('div');
+  let div6El = document.createElement('div');
   div5El.appendChild(div6El);
-  div6El.className='textarea';
-  let brAtt= document.createElement('br');
+  div6El.className = 'textarea';
+  let brAtt = document.createElement('br');
   div5El.appendChild(brAtt);
-  let div7El=document.createElement('div');
+  let div7El = document.createElement('div');
   div5El.appendChild(div7El);
-  div7El.className='btn';
-  let buttonEl=document.createElement('button');
+  div7El.className = 'btn';
+  let buttonEl = document.createElement('button');
   div7El.appendChild(buttonEl);
-  buttonEl.type='submit';
-  buttonEl.textContent='Post';
+  buttonEl.type = 'submit';
+  buttonEl.textContent = 'Post';
   let div8El = document.createElement('div');
   div5El.appendChild(div8El);
-  div8El.id='star';
+  div8El.id = 'star';
 };
 
 
